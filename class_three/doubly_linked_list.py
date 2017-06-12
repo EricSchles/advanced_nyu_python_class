cc97dad16247863585e5f0078db04033ac896d27
Eric Schles <ericschles@gmail.com>
2017-06-12 18:56:54 -0400

    incomplete doubly linked list and complete singly linked list

---
+++ class_three/doubly_linked_list.py
@@ -0,0 +1,51 @@
+# psuedo code version
+# record DoublyLinkedNode {
+#     prev // A reference to the previous node
+#     next // A reference to the next node
+#     data // Data or a reference to data
+# }
+# code for this psuedo code below
+
+class DoublyLinkedNode:
+    def __init__(self, data, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return repr(self.data)
+
+# record DoublyLinkedList {
+#      DoublyLinkedNode firstNode   // points to first node of list
+#      DoublyLinkedNode lastNode    // points to last node of list
+# }
+# code for this psuedo code below
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.first_node = None
+        self.last_node = None
+
+    def traverse_forward(self):
+        node = self.first_node
+        while node is not None:
+            print(node.data)
+            node = node.next
+
+    def traverse_backward(self):
+        node = self.last_node
+        while node is not None:
+            print(node.data)
+            node = node.prev
+
+    def insert_after(self, node, new_node):
+        new_node.prev = node
+        if node.next is None:
+            new_node.next = None
+            self.last_node = new_node
+        else:
+            new_node.next = node.next
+            node.next.prev = new_node
+        node.next = new_node
+
+    
---
+++ class_three/singly_linked_list.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+    def __str__(self):
+        return repr(self.data)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def append(self, data):
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = Node(data)
+        self.size += 1
+        
+    def __getitem__(self, position):
+        counter = 0
+        if position < self.size: 
+            cur = self.head
+            while counter != position:
+                cur = cur.next
+                counter += 1
+            return cur.data
+        else:
+            raise ValueError("position is outside of the list")
+        
+
