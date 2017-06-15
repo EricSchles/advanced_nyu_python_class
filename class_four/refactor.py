692a7bbd4c0ce05a249659a5053842ff80291d3f
Eric Schles <ericschles@gmail.com>
2017-06-15 18:40:59 -0400

    refactor complete

--- class_four/refactor.py
+++ class_four/refactor.py
@@ -1,3 +1,36 @@
+def combine_number_reps(decimal_rep,
+                        binary_width,
+                        octal_rep, hex_rep,
+                        binary_rep):
+    """
+    Description:
+    
+    Combine all four representations of the number.
+    Representations we are combining:
+    * decimal,
+    * octal,
+    * hexidecimal
+    * binary
+    
+    Parameters
+    ----------
+    * decimal_rep - The decimal representation of the number
+    e.g. 0.5
+    * octal_rep - the octal representation of the number
+    e.g. 10%8 = 12
+    * hex_rep - the hexidecimal representation of the number
+    e.g. 10A
+    * binary_rep - the binary representation of the number
+    e.g 0100
+    * binary_width - the binary width of the number
+    e.g. len(0100), so 4
+    """
+    result = decimal_rep.rjust(binary_width)
+    result += octal_rep.rjust(binary_width+1)
+    result += hex_rep.rjust(binary_width+1)
+    result += binary_rep.rjust(binary_width+1)
+    return result
+            
 def print_formatted(number):
     binary_width = len(str(bin(number)[2:]))
     for index in range(1, number+1):
@@ -5,9 +38,5 @@ def print_formatted(number):
         octal_rep = str(int(oct(index)))
         hex_rep = str(hex(index)[2:].upper())
         binary_rep = str(bin(index)[2:])
-        print(
-            decimal_rep.rjust(binary_width)
-            + octal_rep.rjust(binary_width+1)
-            + hex_rep.rjust(binary_width+1)
-            + binary_rep.rjust(binary_width+1)
-        )
+        print(combine_number_reps)
+
